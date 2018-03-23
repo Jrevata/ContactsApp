5c8539a58b34c4b3c199dceef8e139dfdb20a193
Jrevata <jordan.revata@tecsup.edu.pe>
2018-03-23 14:45:06 -0500

    Segundo cambio

---
+++ app/src/main/java/com/jrevata/contactsapp/adapters/PersonAdapter.java
@@ -0,0 +1,40 @@
+package com.jrevata.contactsapp.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by Alumno on 23/03/2018.
+ */
+
+public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.ViewHolder>{
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+
+
+
+}
